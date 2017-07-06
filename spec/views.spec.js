af226011871331cd57cade9bc8c193c00fb66a33
Alexander Zou <zou.alexander@gmail.com>
2017-07-06 15:31:32 -0700

    Create initial TodosView, add template to TodoView with tests
    Use fixtures to model the template

--- spec/views.spec.js
+++ spec/views.spec.js
@@ -1,11 +1,33 @@
 describe('TodoView', () => {
-  let todoView;
+  let todoView, todoModel;
 
   beforeEach(() => {
-    todoView = new TodoView();
+    loadFixtures('fixtures.html');
+    todoModel = new Backbone.Model({
+      title: 'Foo'
+    });
+    todoView = new TodoView({ model: todoModel });
   });
 
   it('should have an HTML element which is a list element', () => {
     expect(todoView.el.nodeName).toEqual('LI');
   });
+
+  it('should produce HTML that reflects the model', () => {
+    $('ul.todo-list-test').append(todoView.render().el);
+    expect($(todoView.el).find('p')).toHaveText('Foo');
+  });
+});
+
+describe('TodosView', () => {
+  let todosView;
+
+  beforeEach(() => {
+    todosView = new TodosView();
+  });
+
+  it('should have an HTML element which is a list container element', () => {
+    expect(todosView.el.nodeName).toEqual('UL');
+  });
+
 });
--- src/views.js
+++ src/views.js
@@ -1,3 +1,14 @@
 const TodoView = Backbone.View.extend({
-  tagName: 'li'
+  tagName: 'li',
+  initialize: function() {
+    this.template = _.template($('#todo-template').html());
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+});
+
+const TodosView = Backbone.View.extend({
+  tagName: 'ul'
 });
